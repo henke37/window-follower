3e8a7334ca7d83c16040448956ef540fd437eee5
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-12-23 16:11:31 +0100

    Minimal stubs for files that will be written.

---
+++ src/window-follower/getHWND.c
@@ -0,0 +1,8 @@
+#include "getHWND.h"
+#include "capture-internals.h"
+
+#include <stdlib.h>
+
+HWND* GetHWND() {
+	return NULL;
+}
---
+++ src/window-follower/getHWND.h
@@ -0,0 +1,10 @@
+
+#ifndef GET_HWND_H
+#define GET_HWND_H
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+HWND* GetHWND();
+
+#endif
