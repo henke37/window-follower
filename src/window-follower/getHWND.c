3815066efe44b1809a015214d0518d41e97f412f
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-12-24 01:19:03 +0100

    Working GetHWND implementation

--- src/window-follower/getHWND.c
+++ src/window-follower/getHWND.c
@@ -2,8 +2,24 @@
 #include "capture-internals.h"
 
 #include <stdlib.h>
+#include <obs-internal.h>
 
 HWND* GetHWND(obs_source_t *source) {
+
+	if (!source) return NULL;
+
+	void *data=source->context.data;
+	const char* id = obs_source_get_id(source);
+
+	if (strcmp(id, "game_capture") == 0) {
+		struct game_capture* gcData = (struct game_capture*)data;
+		return &gcData->window;
+	}
+	if (strcmp(id, "window_capture") == 0) {
+		struct window_capture* wcData = (struct window_capture*)data;
+		return &wcData->window;
+	}
+
 	return NULL;
 }
 
