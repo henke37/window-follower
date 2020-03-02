f7256320db7e0715e8a7901e0fe1883ae7647941
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-03-02 11:07:41 +0100

    Quick and dirty batchfile to copy the build for testing

---
+++ copyFiles.bat
@@ -0,0 +1,4 @@
+copy build\src\window-follower\Debug\*.dll ..\obs-studio\debug64\rundir\Debug\obs-plugins\64bit
+copy build\src\window-follower\Debug\*.pdb ..\obs-studio\debug64\rundir\Debug\obs-plugins\64bit
+
+copy data\window-follower\*.ini ..\obs-studio\debug64\rundir\Debug\data\obs-plugins\window-follower\locale
