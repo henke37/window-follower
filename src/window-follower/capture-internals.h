f81b83da3381da2f299c059885d93095c57b9304
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-03-03 07:29:33 +0100

    Update capture-internals.h to match new versions of the structs used for winrt capture support.

--- src/window-follower/capture-internals.h
+++ src/window-follower/capture-internals.h
@@ -10,7 +10,7 @@
 #include <ipc-util/pipe.h>
 
 struct dc_capture {
-	gs_texture_t* texture;
+	gs_texture_t *texture;
 	bool texture_written;
 	int x, y;
 	uint32_t width;
@@ -19,7 +19,7 @@ struct dc_capture {
 	bool compatibility;
 	HDC hdc;
 	HBITMAP bmp, old_bmp;
-	BYTE* bits;
+	BYTE *bits;
 
 	bool capture_cursor;
 	bool cursor_captured;
@@ -29,18 +29,42 @@ struct dc_capture {
 	bool valid;
 };
 
-struct window_capture {
-	obs_source_t* source;
+struct winrt_exports {
+	bool *(*winrt_capture_supported)();
+	struct winrt_capture *(*winrt_capture_init)(bool cursor, HWND window,
+		bool client_area);
+	void (*winrt_capture_free)(struct winrt_capture *capture);
+	void (*winrt_capture_render)(struct winrt_capture *capture,
+		gs_effect_t *effect);
+	uint32_t(*winrt_capture_width)(const struct winrt_capture *capture);
+	uint32_t(*winrt_capture_height)(const struct winrt_capture *capture);
+};
 
-	char* title;
-	char* class;
-	char* executable;
+enum window_capture_method {
+	METHOD_AUTO,
+	METHOD_BITBLT,
+	METHOD_WGC,
+};
+
+struct window_capture {
+	obs_source_t *source;
+
+	char *title;
+	char *class;
+	char *executable;
+	enum window_capture_method method;
 	enum window_priority priority;
 	bool cursor;
 	bool compatibility;
+	bool client_area;
 	bool use_wildcards; /* TODO */
 
 	struct dc_capture capture;
+
+	bool wgc_supported;
+	void *winrt_module;
+	struct winrt_exports exports;
+	struct winrt_capture *capture_winrt;
 
 	float resize_timer;
 	float check_window_timer;
@@ -51,13 +75,13 @@ struct window_capture {
 };
 
 struct cached_cursor {
-	gs_texture_t* texture;
+	gs_texture_t *texture;
 	uint32_t cx;
 	uint32_t cy;
 };
 
 struct cursor_data {
-	gs_texture_t* texture;
+	gs_texture_t *texture;
 	HCURSOR current_cursor;
 	POINT cursor_pos;
 	long x_hotspot;
@@ -85,9 +109,9 @@ enum hook_rate {
 };
 
 struct game_capture_config {
-	char* title;
-	char* class;
-	char* executable;
+	char *title;
+	char *class;
+	char *executable;
 	enum window_priority priority;
 	enum capture_mode mode;
 	uint32_t scale_cx;
@@ -103,7 +127,7 @@ struct game_capture_config {
 };
 
 struct game_capture {
-	obs_source_t* source;
+	obs_source_t *source;
 
 	struct cursor_data cursor_data;
 	HANDLE injector_process;
@@ -141,8 +165,8 @@ struct game_capture {
 	struct game_capture_config config;
 
 	ipc_pipe_server_t pipe;
-	gs_texture_t* texture;
-	struct hook_info* global_hook_info;
+	gs_texture_t *texture;
+	struct hook_info *global_hook_info;
 	HANDLE keepalive_mutex;
 	HANDLE hook_init;
 	HANDLE hook_restart;
@@ -153,21 +177,21 @@ struct game_capture {
 	HANDLE global_hook_info_map;
 	HANDLE target_process;
 	HANDLE texture_mutexes[2];
-	wchar_t* app_sid;
+	wchar_t *app_sid;
 	int retrying;
 	float cursor_check_time;
 
 	union {
 		struct {
-			struct shmem_data* shmem_data;
-			uint8_t* texture_buffers[2];
+			struct shmem_data *shmem_data;
+			uint8_t *texture_buffers[2];
 		};
 
-		struct shtex_data* shtex_data;
-		void* data;
+		struct shtex_data *shtex_data;
+		void *data;
 	};
 
-	void (*copy_texture)(struct game_capture*);
+	void (*copy_texture)(struct game_capture *);
 };
 
 #endif
