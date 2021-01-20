b2979b51e9e9a978d20e9aa792d6b9c02c8c276a
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-01-20 09:40:09 +0100

    Copy 32 bit files too

--- copyFiles.bat
+++ copyFiles.bat
@@ -1,6 +1,9 @@
 copy Debug64\Debug\*.dll ..\obs-studio\debug64\rundir\Debug\obs-plugins\64bit
 copy Debug64\Debug\*.pdb ..\obs-studio\debug64\rundir\Debug\obs-plugins\64bit
+copy Debug32\Debug\*.dll ..\obs-studio\debug32\rundir\Debug\obs-plugins\32bit
+copy Debug32\Debug\*.pdb ..\obs-studio\debug32\rundir\Debug\obs-plugins\32bit
 
 copy data\locale\*.ini ..\obs-studio\debug64\rundir\Debug\data\obs-plugins\window-follower\locale
+copy data\locale\*.ini ..\obs-studio\debug32\rundir\Debug\data\obs-plugins\window-follower\locale
 
 pause
