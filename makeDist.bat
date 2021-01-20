11765a572ed9bfc1bf3673b90c1d10e56ef0b9c8
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-01-20 09:46:56 +0100

    Automate dist generation

---
+++ makeDist.bat
@@ -0,0 +1,8 @@
+copy Debug64\Debug\*.dll dist\obs-plugins\64bit
+copy Debug64\Debug\*.pdb dist\obs-plugins\64bit
+copy Debug32\Debug\*.dll dist\obs-plugins\32bit
+copy Debug32\Debug\*.pdb dist\obs-plugins\32bit
+
+copy data\locale\*.ini dist\data\obs-plugins\window-follower\locale
+
+pause
