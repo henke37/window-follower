d75c448f3cb2e36b65952c33805c7e84a0c1464e
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-01-31 05:15:06 +0100

    Update the dist building for release builds

---
+++ makeDebugDist.bat
@@ -0,0 +1,8 @@
+copy Debug64\Debug\*.dll distDebug\obs-plugins\64bit
+copy Debug64\Debug\*.pdb distDebug\obs-plugins\64bit
+copy Debug32\Debug\*.dll distDebug\obs-plugins\32bit
+copy Debug32\Debug\*.pdb distDebug\obs-plugins\32bit
+
+copy data\locale\*.ini distDebug\data\obs-plugins\window-follower\locale
+
+pause
